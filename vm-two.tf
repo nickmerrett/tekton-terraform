566cf8efe0ad0c4244d6d9a9101654720e390eae
nickmerrett <nick.merrett@ibm.com>
2021-09-28 15:32:53 +1000

    added an extra vm

--- vm-two.tf
+++ vm-two.tf
@@ -1,50 +1,9 @@
 # Basic configuration without variables
 
-# Define authentification configuration
-provider "vsphere" {
-  # If you use a domain set your login like this "Domain\\User"
-  user           = "administrator@vsphere.local"
-  password       = "HomeLab23!"
-  vsphere_server = "10.20.20.196"
-
-  # If you have a self-signed cert
-  allow_unverified_ssl = true
-}
-
-#### RETRIEVE DATA INFORMATION ON VCENTER ####
-
-data "vsphere_datacenter" "dc" {
-  name = "Datacenter"
-}
-
-# If you don't have any resource pools, put "Resources" after cluster name
-data "vsphere_resource_pool" "pool" {
-  name          = "Homelab/Resources"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-# Retrieve datastore information on vsphere
-data "vsphere_datastore" "datastore" {
-  name          = "datastore3"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-# Retrieve network information on vsphere
-data "vsphere_network" "network" {
-  name          = "VM Network"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-# Retrieve template information on vsphere
-data "vsphere_virtual_machine" "template" {
-  name          = "debian10"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
 #### VM CREATION ####
 
 # Set vm parameters
-resource "vsphere_virtual_machine" "demo" {
+resource "vsphere_virtual_machine" "vm-two" {
   name             = "vm-two"
   num_cpus         = 1 
   memory           = 1024
